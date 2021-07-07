f8d4410bbdf336528704840641cb7243733ec468
Sean Phillips <seanphillips@Seans-Air.broadband>
2021-07-07 15:15:12 +0100

    adding html

--- src/thermostat.js
+++ src/thermostat.js
@@ -6,6 +6,8 @@ class Thermostat {
     this.MAX_LIMIT_PSM_OFF = 32;
     this.MINIMUM_TEMPERATURE = 10;
     this.DEFAULT_TEMPERATURE = 20;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
     this.temperature = this.DEFAULT_TEMPERATURE;
     this.powerSavingMode = true;
   }
@@ -45,5 +47,13 @@ class Thermostat {
   resetTemperature() {
     this.temperature = this.DEFAULT_TEMPERATURE;
   }
-
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+      return 'medium-usage';
+    }
+    return 'high-usage';
+  }
 };
